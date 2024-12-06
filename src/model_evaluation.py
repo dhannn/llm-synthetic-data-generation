f1642750a1031921c801756259655954db5ec641
Daniel Ramos <daniel_ramos@dlsu.edu.ph>
2024-12-06 15:30:19 +0800

    Polish code

--- src/model_evaluation.py
+++ src/model_evaluation.py
@@ -1,24 +1,50 @@
 from sklearn.base import BaseEstimator
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, roc_auc_score, confusion_matrix, f1_score
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold
+
+import numpy as np
+import pandas as pd
 
 class ModelEvaluator:
 
     model: BaseEstimator
     pipeline: Pipeline
+    kf: KFold
 
-    def __init__(self, model: BaseEstimator, X_test, y_test) -> None:
-        self.X_test = X_test
-        self.y_test = y_test
+    def __init__(self, model: BaseEstimator, k=6, random_state=None) -> None:
         self.model = model
         self.pipeline = Pipeline([
             ('vectorizer', TfidfVectorizer()),
             ('classifier', model)    
-        ], verbose=True)
+        ])
 
-    def evaluate(self, X, y):
-        self.pipeline.fit(X, y)
-        y_pred = self.pipeline.predict(self.X_test)
-        accuracy = accuracy_score(self.y_test, y_pred)        
-        return y_pred, accuracy
+        if random_state != None:
+            np.random.seed(random_state)
+            self.kf = KFold(n_splits=k, shuffle=True, random_state=42)
+
+    def evaluate(self, X: pd.DataFrame, y: pd.Series, has_synthetic=False):
+        # return cross_val_score(self.pipeline, X, y)
+        real_X = X.groupby('real').get_group(1).drop('real', axis=1)
+        syn_X = X.groupby('real').get_group(0).drop('real', axis=1) if has_synthetic else pd.DataFrame()
+        syn_y = y[syn_X.index] if has_synthetic else pd.DataFrame()
+
+        scores = []
+        for t, v in self.kf.split(real_X):
+            X_train, X_val = real_X.iloc[t], real_X.iloc[v]
+            y_train, y_val = y.iloc[t], y.iloc[v]   
+
+            if has_synthetic:
+                X_train = pd.concat((X_train, syn_X), ignore_index=True)
+                y_train = pd.concat((y_train, syn_y), ignore_index=True)
+            
+            self.pipeline.fit(np.array(X_train).reshape(-1), y_train)
+            y_pred = self.pipeline.predict(np.reshape(X_val, -1))
+            
+            acc = f1_score(y_val, y_pred)
+            scores.append(acc)
+            
+        return scores
+
